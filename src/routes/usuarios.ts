069268458f5b951136704c29fb2bf076588e79d2
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-17 21:54:59 -0300

    feat: :tada: start proyect

---
+++ src/routes/usuarios.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import {
+	getUsers,
+	getUser,
+	postUser,
+	putUser,
+	deleteUser,
+} from '../controller/usuarios';
+
+const usersRouter = Router();
+
+usersRouter.get('/', getUsers);
+
+usersRouter.get('/:id', getUser);
+
+usersRouter.post('/', postUser);
+
+usersRouter.put('/:id', putUser);
+
+usersRouter.delete('/:id', deleteUser);
+
+export default usersRouter;
