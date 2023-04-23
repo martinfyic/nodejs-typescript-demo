972f30ad402ff0395ac925a6e8d65cf85905d279
Martin Ferreira <martin.f.yic@gmail.com>
2023-04-23 19:01:49 -0300

    feat: :sparkles: add src/database/connections.ts

---
+++ src/database/connection.ts
@@ -0,0 +1,14 @@
+import { Sequelize } from 'sequelize';
+
+const databaseName = process.env.DB_NAME ?? 'database-name';
+const databaseUser = process.env.DB_USER ?? 'database-user';
+const databasePassword = process.env.DB_PASSWORD ?? 'database-user-password';
+const databaseUrl = process.env.DB_HOST ?? 'database-url';
+
+const db = new Sequelize(databaseName, databaseUser, databasePassword, {
+	host: databaseUrl,
+	dialect: 'mysql',
+	// logging: false,
+});
+
+export default db;
